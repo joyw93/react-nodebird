e2aec613d9c5ebdffff9c932fa01b6d480cf98b9
unknown <joyw93@naver.com>
2022-06-03 21:17:54 +0900

    redux 설치

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,18 +1,19 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
 import "antd/dist/antd.css";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 import UserProfile from "../components/UserProfile";
 import LoginForm from "../components/LoginForm";
 
 const SearchInput = styled(Input.Search)`
-vertical-align:middle;`;
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   return (
     <>
       <Menu mode="horizontal">
@@ -37,10 +38,10 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row>
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn}/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
-          {children}
+          {items}
         </Col>
         <Col xs={24} md={6}>
           <a
--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,27 +1,29 @@
-import { useState, useCallback } from "react";
+import { useCallback } from "react";
 import { Form, Input, Button } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
-import useInput from '../hooks/useInput';
+import { useDispatch } from "react-redux";
+import useInput from "../hooks/useInput";
+import { loginAction } from "../reducers";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
 const FormWrapper = styled(Form)`
-padding:  10px;
-`
+  padding: 10px;
+`;
 
-const LoginForm = ({setIsLoggedIn}) => {
-  const [id, onChangeId] = useInput('')
-  const [password, onChangePassword] = useInput('')
-  
+const LoginForm = ({ setIsLoggedIn }) => {
+  const dispatch = useDispatch();
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
-      console.log(id, password)
-      setIsLoggedIn(true)
-  }, [id, password])
-
+    console.log(id, password);
+    dispatch(loginAction({ id, password }));
+  }, [id, password]);
+  
   return (
     <FormWrapper onFinish={onSubmitForm}>
       <div>
