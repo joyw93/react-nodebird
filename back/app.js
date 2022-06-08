542ec43d302d11e521a6fb35bbf37f57f7cc0813
JoYongWon <joyw93@naver.com>
2022-06-08 18:16:00 +0900

    코멘트폼 추가

--- back/app.js
+++ back/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/user");
+
 
 app.use((req, res, next) => {
   console.log("항상실행");
@@ -7,17 +9,17 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-    
-
-});
-
 app.get("/", (req, res) => {
+  let id = req.query.id || 100;
   console.log(req.user);
-  res.send("hello world");
+  res.send(String(id));
 });
 
-
+// app.get("/:id", (req, res) => {
+//   let id = req.params.id || 1;
+//   console.log(id)
+//   res.send(id);
+// });
 
 app.listen(3080, () => {
   console.log("http://127.0.0.1:3080");
